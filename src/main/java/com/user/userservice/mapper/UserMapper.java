d4f0700f3e7e6a27befadd672ace5404ad41794e
Vipin.Sukumaran <Vipin.Sukumaran@host.docker.internal>
2025-01-02 13:17:47 +0530

    adding missed config

--- src/main/java/com/user/userservice/mapper/UserMapper.java
+++ src/main/java/com/user/userservice/mapper/UserMapper.java
@@ -6,7 +6,7 @@ import com.user.userservice.dto.requestDto.UserRequestDto;
 import com.user.userservice.dto.responseDto.UserResponseDto;
 import com.user.userservice.entity.User;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 	User toEntity(UserRequestDto reqDto);
 	UserResponseDto toDto(User course);
